645fce0fd6bede855893ca51d80d6063a941cd73
AQUINRU <russel.aquino@oocl.com>
2021-08-16 19:22:01 +0800

    [Counter][Russel] feat: dynamic MultipleCounter

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,10 +1,20 @@
-function CounterSizeGenerator() {
+import { useState } from "react";
+
+function CounterSizeGenerator(props) {
+
+    const [size, setSize] = useState(0);
+
+    function handleChangeSize(event) {
+        setSize(parseInt(event.target.value));
+        props.updateCounterSize(parseInt(event.target.value));
+    }
+
     return (
         <div>
             <span>Size: </span>
-            <input type="number" min="0" defaultValue="0"></input>
+            <input type="number" min="0" defaultValue="0" value={size} onChange={handleChangeSize}></input>
         </div>
-    )
+    );
 }
 
 export default CounterSizeGenerator;
