92ef20df3ab7694df9512d6f1316548019aefcef
AQUINRU <russel.aquino@oocl.com>
2021-08-16 19:04:43 +0800

    [Counter][Russel] feat: created CounterSizeGenerator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CounterSizeGenerator from './components/CounterSizeGenerator';
 import CounterGroup from './components/CounterGroup';
 
 function App() {
   return (
       <div className="App">
+          <CounterSizeGenerator></CounterSizeGenerator>
           <CounterGroup></CounterGroup>
     </div>
   );
---
+++ src/components/CounterSizeGenerator.js
@@ -0,0 +1,10 @@
+function CounterSizeGenerator() {
+    return (
+        <div>
+            <span>Size: </span>
+            <input type="number" min="0" defaultValue="0"></input>
+        </div>
+    )
+}
+
+export default CounterSizeGenerator;
