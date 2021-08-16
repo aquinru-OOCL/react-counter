75d2c926fc8e56df715fa9adeaef0c179a0e6c2f
AQUINRU <russel.aquino@oocl.com>
2021-08-16 19:00:51 +0800

    [Counter][Russel] feat: created CounterGroup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './components/Counter';
+import CounterGroup from './components/CounterGroup';
 
 function App() {
   return (
       <div className="App">
-          <Counter></Counter>
+          <CounterGroup></CounterGroup>
     </div>
   );
 }
